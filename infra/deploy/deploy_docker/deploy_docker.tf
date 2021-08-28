224965d003558394c7a2807426bda5718288f1aa
nhannguyentrong <nhannguyentrong@msn.com>
2021-08-28 17:41:29 +0000

    Update module name and module path

--- infra/deploy/deploy_docker/deploy_docker.tf
+++ infra/deploy/deploy_docker/deploy_docker.tf
@@ -1,5 +1,5 @@
-module "ecr_build" {
-  source              = "../../modules/ecr"
+module "deploy_docker" {
+  source              = "../../modules/deploy_docker"
   erc_repository_name = var.erc_repository_name
   code_build_project  = var.code_build_project
   repository_name     = var.repository_name
